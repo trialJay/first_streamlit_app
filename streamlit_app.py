b4e94fc502c0ffb35175d2ac3a784c12f2e5a73c
trialJay <131354326+trialJay@users.noreply.github.com>
2023-04-21 14:43:31 +0530

    added user input for fruit name to get response from fruityvice

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,10 +17,13 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 fruits_selected = streamlit.multiselect("Pick some fruits:",list(my_fruit_list.index),['Avocado','Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
-streamlit.header("Fruityvice Fruit Advice!")
 
+
+streamlit.header("Fruityvice Fruit Advice!")
+fruit_choice = streamlit.text_input('What fruit would you like informed about ?','Kiwi')
+streamlit.write('The user entered',fruit_choice)
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
  # streamlit.text(fruityvice_response.json())
 
 # take json version and normalize it 
